5dc70f1f5a9f4d1e895b8025574a68fb4b3429c6
Roxanne Gutierrez <roxannenguyenn@gmail.com>
2023-03-10 09:10:55 -0600

    reviewed lesson

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,13 +10,13 @@ const linkStyles = {
   color: "white",
 };
 
-function Navbar({ setIsLoggedIn }) {
+function Navbar({ onLogout }) {
   const history = useHistory();
 
-  function handleLogout() {
-    setIsLoggedIn(false);
+  const handleClick = () => {
+    onLogout();
     history.push("/login");
-  }
+  };
 
   return (
     <div>
@@ -53,7 +53,7 @@ function Navbar({ setIsLoggedIn }) {
       >
         Login
       </NavLink>
-      <button onClick={handleLogout}>Logout</button>
+      <button onClick={handleClick}>Logout</button>
     </div>
   );
 }
